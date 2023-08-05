9ce33799f339501598631e4b5a0b15582e36cb36
WillyAgustri <willyofficial082@gmail.com>
2023-08-05 16:26:38 +0700

    add Cart and format currency

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
     }
 
     /**
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+        Blade::directive('money', function ($amount) {
+            return "<?php echo 'Rp.' . number_format($amount, 2); ?>";
+});
+}
 }
